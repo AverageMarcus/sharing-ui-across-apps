da15cb04e5673c9bd609f80140d3101858a898b4
Marcus Noble <marcus@marcusnoble.co.uk>
2017-10-17 18:56:09 +0100

    🚀

---
+++ README.md
@@ -0,0 +1,10 @@
+# Sharing UI across multiple Node applications
+
+> nginx ESI referenced within the node app's template
+
+## How to run
+
+1. Install docker and docker-compose
+2. `docker-compose build`
+3. `docker-compose up`
+4. Navigate to http://localhost:8080/app1 or http://localhost:8080/app2
---
+++ app2/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use('/',(req, res, next) => {
+  console.log(req.url);
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(process.env.PORT || 3002, () => {
+  console.log(`Running on  http://localhost:${process.env.PORT || 3002}`);
+});
